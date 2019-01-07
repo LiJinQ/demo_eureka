a9947d9f1356d1b3b2b865d871ffebf9ae4b02a1
LiJinQ <1464435332@qq.com>
2019-01-07 17:07:06 +0800

    高可用注册中心v0.0.1

---
+++ src/main/java/com/ljq/demo_eureka/DemoEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.ljq.demo_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LJQ
+ */
+@SpringBootApplication
+public class DemoEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaApplication.class, args);
+    }
+
+}
+
