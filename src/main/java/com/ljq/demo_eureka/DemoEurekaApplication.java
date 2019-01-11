80aa476f9502711f1a1fd117d8990f2f92cc45f4
LiJinQ <1464435332@qq.com>
2019-01-11 20:11:55 +0800

    注册中心v0.0.5

--- src/main/java/com/ljq/demo_eureka/DemoEurekaApplication.java
+++ src/main/java/com/ljq/demo_eureka/DemoEurekaApplication.java
@@ -2,10 +2,12 @@ package com.ljq.demo_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author LJQ
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class DemoEurekaApplication {
 
